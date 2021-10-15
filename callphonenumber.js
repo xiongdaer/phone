6df574af23d98ab9f8286ccac9b8174c9a07e4d9
xiongdaer <92571507+xiongdaer@users.noreply.github.com>
2021-10-15 15:46:47 +0800

    1.0.0

---
+++ callphonenumber.js
@@ -0,0 +1,13 @@
+var callphonenumber = '400-1234-5678';
+(function(window, $, undefined) {
+    $.fn.callphonenumber = function(option) {
+        var options = $.extend({}, option);
+        if (options.phone) {
+            callphonenumber = options.phone;
+        }
+        if (options.text) {
+            $(this).html(options.text.replace(/{{callphonenumber}}/ig, callphonenumber));
+        }
+        $(this).attr('href', ['tel', ':', callphonenumber].join(''));
+    };
+})(window, jQuery);
